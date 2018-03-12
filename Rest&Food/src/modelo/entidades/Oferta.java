06414345c431c3f92ebac5cb07c2a8c003975506
Javi <>
2018-03-12 13:55:52 +0100

    JFrame editarOferta

--- Rest&Food/src/modelo/entidades/Oferta.java
+++ Rest&Food/src/modelo/entidades/Oferta.java
@@ -1,5 +1,47 @@
 package modelo.entidades;
 
+import java.util.Date;
+
 public class Oferta {
+    
+    private final String titulo;
+    private final String descripcion;
+    private final Date fechaInicio;
+    private final Date fechaFinal;
+    private final String tipoOferta;
+    private final String empresa;
+
+    public Oferta(String titulo, String descripcion, Date fechaInicio, Date fechaFinal, String tipoOferta, String empresa) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.tipoOferta = tipoOferta;
+        this.empresa = empresa;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public String getTipoOferta() {
+        return tipoOferta;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
     
 }
