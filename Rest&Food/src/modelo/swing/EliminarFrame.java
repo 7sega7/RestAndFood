edd129b7c5458fda7182830a7aea46fa52d520c9
Grupo 1 Java <Grupo 1 Java@DESKTOP-RM26F3M>
2018-03-12 13:56:53 +0100

    eliminarFrame

--- Rest&Food/src/modelo/swing/EliminarFrame.java
+++ Rest&Food/src/modelo/swing/EliminarFrame.java
@@ -1,19 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo.swing;
 
+import java.awt.BorderLayout;
+import java.awt.ScrollPane;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import modelo.entidades.Oferta;
 
-/**
- *
- * @author Grupo 1 Java
- */
+
+
 public class EliminarFrame {
     
     public static JFrame eliminarFrame(){
+        
+        // Titulos de la parte superior
+        JLabel principal = new JLabel("ELIMINAR OFERTA");
+        
+        principal.setSize(20, 20);
+        
+        JLabel titulo = new JLabel("ELIJA LA OFERTA A ELIMINAR");
+        
+        
+        // Creacion de la Lista
+        JList<Oferta> lista = new JList<>();
+        
+        JPanel panel = new JPanel(new BorderLayout(5, 5));
+        
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
+        // Creacion del boton eliminar
+        JButton buttonDel = new JButton("Eliminar");
+        
+        JPanel inferior = new JPanel();
+        
+        inferior.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
+        inferior.add(buttonDel);
+        
+        // Evento que confirma la eliminacion seleccionada. buttonDel.addActionListener(al);
+        
+        panel.add(principal, BorderLayout.NORTH);
+        panel.add(titulo, BorderLayout.NORTH);
+        panel.add(new JScrollPane(lista), BorderLayout.CENTER );
+        panel.add(inferior, BorderLayout.SOUTH);
+        
+        
+         
         
         return null;
     }
